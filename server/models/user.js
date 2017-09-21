46b0144b91f5974979914cee99c0264e844e4ffb
oleeskild <oleeskilds@gmail.com>
2017-09-21 14:09:35 +0200

    Added authentication for API

---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+  fullName:{
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  resetPasswordToken: {type: String},
+  resetPasswordExpires: {type: Date}
+});
+
+userSchema.pre('save', function(next){
+  const user = this,
+    SALT_FACTOR = 5;
+
+  if(!user.isModified('password')){
+    return next();
+  }
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+    if(err){
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if(err){
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    })
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb){
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if(err){
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server/routes/router.js
@@ -0,0 +1,30 @@
+const AuthenticationController = require('./api/controllers/authentication'),
+  express = require('express'),
+  passportService = require('../config/passport'),
+  passport = require('passport');
+
+// Middleware to require login/auth
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireLogin = passport.authenticate('local', { session: false });
+
+module.exports = function(app) {
+  // Initializing route groups
+  const authRoutes = express.Router();
+  var apiRoutes = express.Router();
+  var api = require('./api/api');
+  //=========================
+  // Auth Routes
+  //=========================
+
+  // Registration route
+  authRoutes.post('/register', AuthenticationController.register);
+
+  // Login route
+  authRoutes.post('/login', requireLogin, AuthenticationController.login);
+
+  // Set auth routes as subgroup/middleware to apiRoutes
+  app.use('/auth', authRoutes);
+
+  // Set url for API group routes
+  app.use('/api', requireAuth, api);
+};
