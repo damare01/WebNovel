c363cafa6b49198884e4ebdd1a64e9124bdcec6e
oleeskild <oleeskilds@gmail.com>
2017-11-07 12:49:39 +0100

    Added some improvements to navigation when reading chapters

--- src/app/select-next-chapter/select-next-chapter.component.ts
+++ src/app/select-next-chapter/select-next-chapter.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core'
+import {Component, Input, OnChanges, OnInit} from '@angular/core'
 import {ChapterService} from '../chapter.service'
 import {Chapter} from '../../models/chapter'
 
@@ -7,16 +7,19 @@ import {Chapter} from '../../models/chapter'
   templateUrl: './select-next-chapter.component.html',
   styleUrls: ['./select-next-chapter.component.css']
 })
-export class SelectNextChapterComponent implements OnInit {
+export class SelectNextChapterComponent implements OnInit, OnChanges {
 
   @Input() chapterId: string
+  parentChapter: Chapter = new Chapter()
   children: Chapter[] = []
 
   constructor(private _chapterService: ChapterService) {
   }
 
   ngOnInit() {
+    this.children = []
     this._chapterService.getChapter(this.chapterId).subscribe(parentChapter => {
+      this.parentChapter = parentChapter
       let counter = 0
       const tmpChildren = []
       const childrenLength = parentChapter.childrenIds.length
@@ -31,4 +34,7 @@ export class SelectNextChapterComponent implements OnInit {
     })
   }
 
+  ngOnChanges() {
+    this.ngOnInit()
+  }
 }
